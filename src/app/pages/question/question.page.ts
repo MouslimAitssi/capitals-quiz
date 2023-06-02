ff592594d8805e44bd3dd1dba1ce8dbd452d0a8b
mouslimAitssi <mouslimaitssi@gmail.com>
2023-06-02 16:42:42 -0700

    adding total questions | showing the clicked question | reloading the page if no data is found

--- src/app/pages/question/question.page.ts
+++ src/app/pages/question/question.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Choice } from '../../model/choice';
 import { Question } from 'src/app/model/question';
 import { Router } from '@angular/router';
 import { QuestionsService } from 'src/app/services/questions/questions.service';
 import { UserService } from 'src/app/services/user/user.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -12,19 +13,26 @@ import { UserService } from 'src/app/services/user/user.service';
   styleUrls: ['./question.page.scss'],
 })
 export class QuestionPage implements OnInit {
-
-  answersDisplayed: boolean = false;
-  question: Question = new Question("Please Wait...", [], "");
+  anAnswerIsSelected: boolean = false;
+  question: Question;
   openModalName: boolean = false;
   questionNumber: number = 0;
+  totalQuestions: number = 0;
+  selectedChoice: Choice;
+  subscription: Subscription = new Subscription();
 
   constructor(
     private questionsService: QuestionsService,
     private userService: UserService,
-    private router: Router) { }
+    private router: Router
+  ) { 
+    this.totalQuestions = this.questionsService.questions.length;
+    this.question = new Question("Please Wait...", [], "");
+    this.selectedChoice = new Choice("", false);
+  }
 
   ngOnInit() {
-    this.questionsService.currentQuestion.subscribe((number) => {
+    this.subscription = this.questionsService.currentQuestion.subscribe((number) => {
       this.questionNumber = number;
       if(!this.userService.user.getValue().username) {
         this.router.navigateByUrl('start');
@@ -33,28 +41,44 @@ export class QuestionPage implements OnInit {
         this.router.navigateByUrl('end');
       }
       this.question = this.questionsService.questions[this.questionNumber];
-      this.answersDisplayed = false;
     })
+  }
+  ionViewDidEnter() {
+    if(!this.question) {
+      window.location.reload();
+    }
   }
 
   selectAnswer(choice: Choice) {
-    this.answersDisplayed = true;
-    if(choice.isTrue) {
-      let user = this.userService.user.getValue();
-      user.totalPoints++;
-      this.userService.user.next(user);
+    if(!this.anAnswerIsSelected) {
+      this.selectedChoice = choice;
+      this.anAnswerIsSelected = true;
+      if(choice.isTrue) {
+        let user = this.userService.user.getValue();
+        user.totalPoints++;
+        this.userService.user.next(user);
+      }
     }
   }
 
   nextQuestion() {
+    this.anAnswerIsSelected = false;
     this.questionsService.currentQuestion.next(this.questionNumber+1);
   }
 
   getClass(choice: Choice) {
-    if(this.answersDisplayed) {
+    if(this.anAnswerIsSelected) {
+      if(choice === this.selectedChoice) {
+        return choice.isTrue?"truthy": "falsy-clicked";
+      }
+      
       return choice.isTrue?"truthy": "falsy";
     }
     return "";
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
